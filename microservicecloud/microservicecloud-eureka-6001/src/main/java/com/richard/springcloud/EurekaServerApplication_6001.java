d0feedb7d6bc37360f12ff1c9c211c1a7cf3e834
jevonhe <jevon.he@gmail.com>
2018-12-10 22:35:47 +0800

    change something

--- microservicecloud/microservicecloud-eureka-6001/src/main/java/com/richard/springcloud/EurekaServerApplication_6001.java
+++ microservicecloud/microservicecloud-eureka-6001/src/main/java/com/richard/springcloud/EurekaServerApplication_6001.java
@@ -3,7 +3,7 @@ package com.richard.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//Test Update
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication_6001 {
